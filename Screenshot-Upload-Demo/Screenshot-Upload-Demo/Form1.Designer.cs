96eb7af91fe9842d30510107ae57e4a14bdbadaf
Adam Sandman <asandman@taravault.net>
2018-06-29 17:34:25 -0400

    sample code

---
+++ Screenshot-Upload-Demo/Screenshot-Upload-Demo/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Screenshot_Upload_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtProject = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(468, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(90, 45);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(462, 22);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "http://localhost/SpiraTeam";
+            this.txtUrl.TextChanged += new System.EventHandler(this.txtUrl_TextChanged);
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(90, 82);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(262, 22);
+            this.txtLogin.TabIndex = 2;
+            this.txtLogin.Text = "fredbloggs";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(90, 120);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(374, 22);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.Text = "{7A05FD06-83C3-4436-B37F-51BCF0060483}";
+            // 
+            // txtProject
+            // 
+            this.txtProject.Location = new System.Drawing.Point(116, 159);
+            this.txtProject.Name = "txtProject";
+            this.txtProject.Size = new System.Drawing.Size(123, 22);
+            this.txtProject.TabIndex = 4;
+            this.txtProject.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "PR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Url:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Login:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "API Key:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Project:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(90, 204);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(374, 22);
+            this.txtFilename.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Filename:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(468, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 29);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Browse...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 313);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtProject);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Sample Upload of Image to Spira using REST";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtProject;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Screenshot-Upload-Demo/Screenshot-Upload-Demo/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Screenshot_Upload_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            this.txtFilename.Text = this.openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.ShowDialog();
+        }
+
+        /// <summary>
+        /// Uploads the file
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create the full URL
+            string url = this.txtUrl.Text.Trim() + "/Services/v5_0/RestService.svc/projects/{project_id}/documents/file?filename={filename}";
+            string projectId = this.txtProject.Text.Trim();
+            string filename = Path.GetFileName(this.txtFilename.Text);
+            url = url.Replace("{project_id}", projectId);
+            url = url.Replace("{filename}", filename);
+
+            //Create the HTTP Post
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.ContentType = "application/json";
+            request.Method = "POST";
+
+            //Add the authentication
+            string base64Credentials = GetEncodedCredentials();
+            request.Headers.Add("Authorization", "Basic " + base64Credentials);
+
+            //Read the file and turn into JSON byte array
+            byte[] contents = File.ReadAllBytes(this.txtFilename.Text);
+            string json = "[";
+            bool first = true;
+            foreach (byte item in contents)
+            {
+                if (!first)
+                {
+                    json += ",";
+                }
+                else
+                {
+                    first = false;
+                }
+                json += item.ToString();
+            }
+            json += "]";
+            
+            //Add the filename body
+            using (StreamWriter writer = new StreamWriter(request.GetRequestStream()))
+            {
+                //writer.Write("[0,0,0,0,0,0,0,0,0,0,0,0,0]");
+                writer.Write(json);
+            }
+
+            //Fire the query and get the response
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            string result = string.Empty;
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            MessageBox.Show("Done!");
+        }
+
+        /// <summary>
+        /// Gets the base64-encoded login/password
+        /// </summary>
+        /// <returns></returns>
+        private string GetEncodedCredentials()
+        {
+            string mergedCredentials = string.Format("{0}:{1}", this.txtLogin.Text.Trim(), this.txtPassword.Text.Trim());
+            byte[] byteCredentials = UTF8Encoding.UTF8.GetBytes(mergedCredentials);
+            return Convert.ToBase64String(byteCredentials);
+        }
+
+        private void txtUrl_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
